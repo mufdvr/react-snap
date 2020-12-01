fe79c20ba43302036a4a3cefba94aa24c88a11de
dv <dv@debian>
2020-12-02 00:11:05 +0300

    update

--- run.js
+++ run.js
@@ -2,46 +2,17 @@
 
 const url = require("url");
 const { run } = require("./index.js");
-const {
-  reactSnap,
-  homepage,
-  devDependencies,
-  dependencies
-} = require(`${process.cwd()}/package.json`);
 
-const publicUrl = process.env.PUBLIC_URL || homepage;
-
-const reactScriptsVersion = parseInt(
-  (devDependencies && devDependencies["react-scripts"])
-  || (dependencies && dependencies["react-scripts"])
-);
-let fixWebpackChunksIssue;
-switch (reactScriptsVersion) {
-  case 1:
-    fixWebpackChunksIssue = "CRA1";
-    break;
-  case 2:
-    fixWebpackChunksIssue = "CRA2";
-    break;
-}
-
-const parcel = Boolean(
-  (devDependencies && devDependencies["parcel-bundler"])
-  || (dependencies && dependencies["parcel-bundler"])
-);
-
-if (parcel) {
-  if (fixWebpackChunksIssue) {
-    console.log("Detected both Parcel and CRA. Fixing chunk names for CRA!")
-  } else {
-    fixWebpackChunksIssue = "Parcel";
-  }
-}
+const publicUrl = process.env.PUBLIC_URL
 
 run({
   publicPath: publicUrl ? url.parse(publicUrl).pathname : "/",
-  fixWebpackChunksIssue,
-  ...reactSnap
+  inlineCss: true,
+  puppeteerExecutablePath: "/usr/bin/chromium-browser",
+  concurrency: 1,
+  puppeteerArgs: [
+    "--disable-setuid-sandbox", "--no-sandbox"
+  ]
 }).catch(error => {
   console.error(error);
   process.exit(1);
