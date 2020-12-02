f50bef27819e0a9e95e528a75d9a3fd54b634a75
dv <dv@debian>
2020-12-02 19:14:59 +0300

    update

--- run.js
+++ run.js
@@ -1,17 +1,25 @@
 #!/usr/bin/env node
 
 const url = require("url");
+const nativeFs = require("fs");
 const { run } = require("./index.js");
-const {
-  reactSnap
-} = require(`/app/package.json`);
+
+let reactSnap
+if (nativeFs.existsSync("/app/package.json")) {
+  reactSnap  = require("/app/package.json").reactSnap;
+} else {
+  reactSnap = {
+    puppeteerArgs: ["--disable-setuid-sandbox", "--no-sandbox"],
+    inlineCss: false,
+  };
+}
 
 const publicUrl = process.env.PUBLIC_URL;
 
 run({
   publicPath: publicUrl ? url.parse(publicUrl).pathname : "/",
-  ...reactSnap
-}).catch(error => {
+  ...reactSnap,
+}).catch((error) => {
   console.error(error);
   process.exit(1);
 });
